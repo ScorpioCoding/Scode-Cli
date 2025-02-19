3bb0df9f7c7b591e45a2c11b24959409751bf979
ScorpioCoding <dev@scorpiocoding.com>
2025-02-19 12:17:09 +0100

    Initial Setup

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+import ora from "ora";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import * as fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import createDirectoryContents from "./createDirectoryContents.js";
+const CURR_DIR = process.cwd();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
+const QUESTIONS = [
+  {
+    name: "project-choice",
+    type: "list",
+    message: "What project template would you like to generate?",
+    choices: CHOICES,
+  },
+  {
+    name: "project-name",
+    type: "input",
+    message: "Project name:",
+    validate: function (input) {
+      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+      else
+        return "Project name may only include letters, numbers, underscores and hashes.";
+    },
+  },
+];
+
+inquirer.prompt(QUESTIONS).then((answers) => {
+  const projectChoice = answers["project-choice"];
+  const projectName = answers["project-name"];
+  const templatePath = `${__dirname}/templates/${projectChoice}`;
+
+  const spinner = ora(`Doing ${projectChoice}...`).start(); // Start the spinner
+
+  fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+  createDirectoryContents(templatePath, projectName);
+
+  setTimeout(() => {
+    spinner.succeed(chalk.green("Done!"));
+  }, 3000);
+});
